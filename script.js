815f10fb9f353f250a15c29199700a93c54cbbbc
Charles Kondrath <chuk944@aol.com>
2022-04-25 20:27:05 -0700

    04-25-2022

--- script.js
+++ script.js
@@ -5,36 +5,88 @@
 
 
 
-function start() {
-    
+const start = () => {
+    var r = document.querySelector(':root');
     let range = document.getElementById("vol").value;
-    console.log(range);
+    let toggle = document.getElementById('switch');
+    let total = 0;
+    console.log(range);   
+    let newRange = range +"%";
     
-    if (range < 5) {
+    // Slide Bar Tracking
+    r.style.setProperty('--sliderBar', newRange);
+
+    // Main Selection
+
+    // 10k Pageview
+    
+    if (range < 20) {
         range = 10;
         document.getElementById("pageview").innerHTML = "10k pageviews";
+        total = 8.00;
+        total = total.toFixed(2);
+        if (toggle.checked == true) {
+            total = total - (total * .25);
+            total = total.toFixed(2);
+        }
+        document.getElementById('monthCharge').innerHTML = '$' + total;
     }
 
-    else if (range >=5 && range < 20) {
+    // 50k Pageview
+
+    else if (range >=20 && range < 40) {
         range = 50;
         document.getElementById("pageview").innerHTML = "50k pageviews";
-    }
+        total = 12.00;
+        total = total.toFixed(2);
+        if (toggle.checked == true) {
+            total = total - (total * .25);
+            total = total.toFixed(2);
+        }
+        document.getElementById('monthCharge').innerHTML = '$' + total;
 
-    else if (range >=20 && range < 51) {
+
+    }
+    // 100k Pageview
+
+    else if (range >=40 && range < 60) {
         range = 100;
         document.getElementById("pageview").innerHTML = "100k pageviews";
+        total = 16.00;
+        total = total.toFixed(2);
+        if (toggle.checked == true) {
+            total = total - (total * .25);
+            total = total.toFixed(2);
+        }
+        document.getElementById('monthCharge').innerHTML = '$' + total;
     }
 
-    else if (range >=51 && range < 80) {
+    // 500k Pageview
+    else if (range >=60 && range < 80) {
         range = 500;
         document.getElementById("pageview").innerHTML = "500k pageviews";
+        total = 24.00;
+        total = total.toFixed(2);
+        if (toggle.checked == true) {
+            total = total - (total * .25);
+            total = total.toFixed(2);
+        }
+        document.getElementById('monthCharge').innerHTML = '$' + total;
     }
-
+    // 1m Pageview
     else  {
         range = 1000;
         document.getElementById("pageview").innerHTML = "1m pageviews";
+        total = 36.00
+        total = total.toFixed(2);
+        if (toggle.checked == true) {
+            total = total - (total * .25);
+            total = total.toFixed(2);
+        }
+        document.getElementById('monthCharge').innerHTML = '$' + total;
     }
     
 
-    console.log(range);
+    
 }
+
